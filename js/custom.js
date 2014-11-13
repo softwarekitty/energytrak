86b07a4d7e5213bff16b7a162cc0eba05a9a03bc
Carl Chapman <carlallenchapman@gmail.com>
2014-11-13 16:16:29 -0600

    swapped out bar.js for better labeling function

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,20 @@
 window.onload = function () {
-  var r = Raphael("holder"),
+    make_bar_chart("holder");
+};
+
+function make_bar_chart(chart_id){
+  var r = Raphael(chart_id),
           data1 = [[55, 20, 13, 32, 5, 1, 2, 10], [10, 2, 1, 5, 32, 13, 20, 55], [12, 20, 30]],
           data2 = [[55, 20, 13, 32, 5, 1, 2, 10], [10, 2, 1, 5, 32, 13, 20, 55], [12, 20, 30]],
           data3 = [[55, 20, 13, 32, 5, 1, 2, 10], [10, 2, 1, 5, 32, 13, 20, 55], [12, 20, 30]],
           txtattr = { font: "12px 'Fontin Sans', Fontin-Sans, sans-serif" };
 
-  r.text(160, 10, "Single Series Chart").attr(txtattr);
+//  r.text(160, 10, "Single Series Chart").attr(txtattr);
 //          r.text(480, 10, "Multiline Series Chart").attr(txtattr);
 //          r.text(160, 250, "Multiple Series Stacked Chart").attr(txtattr);
 //          r.text(480, 250, 'Multiline Series Stacked Vertical Chart. Type "round"').attr(txtattr);
-  var labels = ["Company X", "Company Y", "Company Z"]
-  var barChart = r.barchart(10, 10, 300, 220, [[9,5,2]], 0, {stacked: true, type: "soft"})//.label([labels]);	  
+//  var labels = ["Company X", "Company Y", "Company Z"]
+  var barChart = r.barchart(10, 10, 300, 220, [[9,5,12]], 0, {stacked: true, type: "soft"}).label([['label1','label2']], true);	  
 
 barChart.addlabels = function () {
 
@@ -20,10 +24,9 @@ console.log(this.bars);
 
 };
 barChart.addlabels();
-
-  //r.barchart(10, 10, 300, 220, [[55, 20, 13, 32, 5, 1, 2, 10]], 0, {type: "sharp"}).label([['C', 'D', 'E', 'G', 'L', 'M', 'P', 'S1']]);
-  //barChart.label(['a','b','c']);
-//          r.barchart(330, 10, 300, 220, data1);
-//          r.barchart(10, 250, 300, 220, data2, {stacked: true});
-//          r.barchart(330, 250, 300, 220, data3, {stacked: true, type: "round"});
-};
+// r.barchart(10, 10, 300, 220, [[55, 20, 13, 32, 5, 1, 2, 10]], 0, {type: "sharp"}).label([['C', 'D', 'E', 'G', 'L', 'M', 'P', 'S1']]);
+// barChart.label(['a','b','c']);
+//           r.barchart(330, 10, 300, 220, data1);
+//           r.barchart(10, 250, 300, 220, data2, {stacked: true});
+//           r.barchart(330, 250, 300, 220, data3, {stacked: true, type: "round"});
+}
